6503c30da98649ce3c8f09564160ee932967732b
mateoh123 <mateoche76@gmail.com>
2024-03-25 06:38:11 -0400

    shaking worsens

--- CCMidterm.js
+++ CCMidterm.js
@@ -9,7 +9,6 @@ var knob = 550;
 var cloudfade = 300;
 var lightson = "#000000";
 
-
 //SCENE TWO VARIABLES//
 var scene2 = false
 var shelvesmove1 = 600
@@ -18,8 +17,14 @@ var bedroommove = 0
 
 //SCENE THREE VARIABLES//
 var scene3 = false
+
+//SCENE FOUR VARIABLES//
+var scene4 = false
 var flash = 0
 var flashcolor = "#000000"
+
+//SCENE SLEEPING BEDROOM VARIABLES//
+var sb_scene = false
 
 function setup() {
   createCanvas(700, 700);
@@ -32,10 +37,7 @@ function setup() {
 }
 
 function draw() {
-  scene_three()
-  flash = flash + 1
 
-  /*
   if (scene1 == true) {
     scene_one();
 
@@ -66,11 +68,6 @@ function draw() {
     if (sun == 100) {
     }
   }
-  
-  if(frameCount == 550){
-    scene1 = false
-    scene2 = true
-  }
 
   if(scene2 == true){
     scene_two()
@@ -93,10 +90,37 @@ function draw() {
       shelvesmove2 = -1000
     }
 
-
   }
 
-  */
+  if(scene3 == true){
+    scene_three()
+  }
+
+  if(sb_scene == true){
+    sleeping_bedroom()
+  }
+
+  if(scene4 == true){
+    scene_four()
+    flash = flash + 1
+  }
+
+  if(frameCount == 550){
+    scene1 = false
+    scene2 = true
+    scene3 = false
+    scene4 = false
+  }else if(frameCount == 1200){
+    scene2 = false
+    scene3 = true
+  }else if(frameCount == 1300){
+    scene3 = false
+    sb_scene = true
+  }else if(frameCount == 1400){
+    sb_scene = false
+    scene4 = true
+  }
+
 
 }
 
@@ -450,6 +474,38 @@ function scene_three(){
 
   //phone
   push();
+  fill(191,148,228)
+  rect(400,200,130,250, 5)
+  fill(flashcolor)
+  rect(405,205,120,240, 5)
+  fill(0, 22, 56)
+  ellipse(465,220,10,10)
+  pop();
+
+  //phone charger
+  push()
+  fill(0,0,0)
+  rect(260,300, 5,400)
+  fill(0,0,0)
+  ellipse(260,330,130,130)
+  fill(255, 194, 28)
+  ellipse(260,330,110,110)
+  fill(0,0,0)
+  ellipse(260,330,100,100)
+  pop()
+
+}
+
+function scene_four(){
+  background(56, 31, 0)
+
+  noStroke()
+  //bedside table
+  fill(66, 36, 0);
+  ellipse(340, 340, 500, 500);
+
+  //phone
+  push();
   translate(random(-5,5),random(-5,5))
   fill(191,148,228)
   rect(400,200,130,250, 5)
@@ -478,4 +534,125 @@ function scene_three(){
     flashcolor = "#000000"
   }
 
+}
+
+function sleeping_bedroom(){
+  angleMode(DEGREES);
+  background(194, 112, 190);
+  push()
+  noStroke();
+  //window
+  fill(74, 72, 72);
+  rect(350, 170, 250, 250);
+  fill(184, 230, 255);
+  rect(360, 180, 230, 230);
+  fill(74, 72, 72);
+  rect(470, 170, 10, 250);
+  rect(460, 390, 5, 30);
+
+  //desk
+  push();
+  translate(-20, 0);
+  push();
+  rotate(20);
+  fill(255, 255, 255);
+  rect(750, 270, 20, 180);
+  rotate(140);
+  rect(-430, -880, 20, 180);
+  pop();
+  fill(207, 148, 74);
+  rect(540, 500, 170, 20);
+  fill(255, 255, 255);
+  rect(585, 600, 80, 20);
+  pop();
+
+  //desk chair
+  fill(235, 235, 235);
+  ellipse(480, 570, 100, 30);
+  ellipse(440, 500, 30, 120);
+  fill(102, 102, 102);
+  rect(420, 570, 110, 20);
+  rect(420, 450, 20, 130);
+  rect(470, 570, 20, 60);
+  rect(465, 630, 30, 40);
+  rect(455, 670, 50, 10);
+
+  //computer and keyboard
+  fill(0, 0, 0);
+  rect(600, 492, 50, 8);
+  rect(620, 450, 8, 50);
+  rect(616, 400, 15, 70);
+  rect(540, 494, 30, 6);
+
+  //pillow
+  fill(255, 255, 255);
+  ellipse(80, 550, 80, 30);
+  //bed
+  fill(255, 255, 255);
+  rect(20, 450, 20, 230);
+  rect(300, 550, 20, 130);
+  rect(20, 600, 280, 20);
+  rect(13, 440, 35, 20);
+  rect(293, 530, 35, 20);
+  fill(255, 214, 250);
+  rect(40, 555, 260, 45, 5);
+
+  //chest
+  fill(66, 36, 0);
+  rect(320, 590, 90, 90);
+  fill(181, 181, 181);
+  rect(410, 620, 10, 20);
+
+  //girl
+  fill(48, 21, 7);
+  ellipse(100, 510, 90, 90);
+  fill(0, 0, 0);
+  rect(85, 480, 5, 15);
+  ellipse(60, 510, 50, 100);
+
+  //bed covers
+  fill(237, 237, 237);
+  ellipse(200, 510, 200, 100);
+  rect(100, 510, 200, 100, 5);
+
+  //bedside table
+  fill(66, 36, 0);
+  rect(30, 565, 90, 15);
+  rect(65, 570, 15, 100);
+  rect(58, 665, 30, 20);
+
+  //phone
+  fill(0, 0, 0);
+  rect(60, 560, 45, 5);
+
+  //shelves
+  fill(255, 255, 255);
+  rect(60, 350, 100, 10);
+  rect(190, 380, 100, 10);
+
+  //posters
+  fill(191, 191, 191);
+  push();
+  rotate(10);
+  rect(100, 100, 80, 120);
+  pop();
+
+  push();
+  rotate(2);
+  rect(200, 180, 80, 120);
+  pop();
+
+  //lights
+  fill(66, 36, 0);
+  rect(300, 0, 100, 15);
+  rect(345, -20, 10, 100);
+  fill(255, 243, 82);
+  ellipse(350, 85, 15, 15);
+  fill(66, 36, 0);
+  rect(300, 77, 100, 5);
+
+  //floor
+  fill(56, 31, 0);
+  rect(0, 680, 700, 30);
+  pop();
 }
